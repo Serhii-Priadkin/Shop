fdc19c4269ad28d11424cf570b04a1a424495e95
Serhii Priadkin <sergio.pryadkin@gmail.com>
2022-08-17 13:33:21 +0300

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+
+namespace Shop
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();// To display page with errors
+            app.UseStatusCodePages();// To display code of page
+            app.UseStaticFiles();// To display static files
+            app.UseMvcWithDefaultRoute();// To use default URL address
+
+        }
+    }
+}
